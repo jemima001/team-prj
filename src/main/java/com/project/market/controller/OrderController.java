e8ce7332e3635da94befa46e25a79e9b4e32a697
eunju <z0303u@gmail.com>
2022-06-09 15:35:38 +0900

    maincontroller -> MainController

---
+++ src/main/java/com/project/market/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.project.market.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class OrderController {
+	
+	private BookingService service;
+	
+	@GetMapping("")
+
+}
---
+++ src/main/java/com/project/market/domain/OrderDto.java
@@ -0,0 +1,5 @@
+package com.project.market.domain;
+
+public class OrderDto {
+
+}
