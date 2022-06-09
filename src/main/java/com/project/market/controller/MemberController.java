2eb6ad8048fccdccfe04f7cd1d3cbca1c68e952d
kbm <hjh564@naver.com>
2022-06-09 16:57:30 +0900

    회원가입 기능 활성화

--- src/main/java/com/project/market/controller/MemberController.java
+++ src/main/java/com/project/market/controller/MemberController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.project.market.domain.MemberDto;
@@ -32,12 +33,51 @@ public class MemberController {
 		
 		if (success) {
 			rttr.addFlashAttribute("message", "회원가입이 완료되었습니다.");
-			return "redirect:/market/project/home";
+			return "redirect:/project/home";
 		} else {
 			rttr.addFlashAttribute("message", "회원가입이 실패하였습니다.");
 			rttr.addFlashAttribute("member", member);
 			
-			return "redirect:/market/member/signup";
+			return "redirect:/member/signup";
+		}
+	}
+	
+	@GetMapping(path = "check", params = "id")
+	@ResponseBody
+	public String idCheck(String id) {
+		
+		boolean exist = service.hasMemberId(id);
+		
+		if (exist) {
+			return "notOk";
+		} else {
+			return "ok";
+		}
+	}
+	
+	@GetMapping(path = "check", params = "email")
+	@ResponseBody
+	public String emailCheck(String email) {
+		
+		boolean exist = service.hasMemberEmail(email);
+		
+		if (exist) {
+			return "notOk";
+		} else {
+			return "ok";
+		}
+	}
+	
+	@GetMapping(path = "check", params = "nickName")
+	@ResponseBody
+	public String nickNameCheck(String nickName) {
+		
+		boolean exist = service.hasMemberNickName(nickName);
+		
+		if (exist) {
+			return "notOk";
+		} else {
+			return "ok";
 		}
 	}
 }
