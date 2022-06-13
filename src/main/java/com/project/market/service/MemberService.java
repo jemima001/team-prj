bde89f3b56238d65b64ecfe72eda4074db69aa71
kbm <hjh564@naver.com>
2022-06-13 14:30:06 +0900

    주소지 추가, 카테고리 수정

--- src/main/java/com/project/market/service/MemberService.java
+++ src/main/java/com/project/market/service/MemberService.java
@@ -10,10 +10,10 @@ import com.project.market.mapper.MemberMapper;
 
 @Service
 public class MemberService {
-	
+
 	@Autowired
 	private MemberMapper mapper;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 
@@ -21,24 +21,24 @@ public class MemberService {
 
 		// 평문암호를 암호화(encoding)
 		String encodedPassword = passwordEncoder.encode(member.getPassword());
-		
+
 		// 암호화된 암호를 다시 세팅
 		member.setPassword(encodedPassword);
-		
-		// insert member  
+
+		// insert member
 		int cnt1 = mapper.insertMember(member);
-		
+
 		// insert auth
 		int cnt2 = mapper.insertAuth(member.getId(), "ROLE_USER");
-				
+
 		return cnt1 == 1 && cnt2 == 1;
-		
+
 	}
 
 	public boolean hasMemberId(String id) {
 		return mapper.countMemberId(id) > 0;
 	}
-	
+
 	public boolean hasMemberEmail(String email) {
 		return mapper.countMemberEmail(email) > 0;
 	}
@@ -51,50 +51,50 @@ public class MemberService {
 		// TODO Auto-generated method stub
 		return mapper.selectMemberById(id);
 	}
-	
+
 	@Transactional
 	public boolean removeMember(MemberDto dto) {
 		MemberDto member = mapper.selectMemberById(dto.getId());
-		
+
 		String rawPW = dto.getPassword();
 		String encodedPW = member.getPassword();
-		
+
 		if (passwordEncoder.matches(rawPW, encodedPW)) {
-					
+
 			// 권한테이블 삭제
 			mapper.deleteAuthById(dto.getId());
-			
+
 			// 멤버테이블 삭제
 			int cnt = mapper.deleteMemberById(dto.getId());
-			
+
 			return cnt == 1;
 		}
-		
+
 		return false;
 	}
 
 	public boolean modifyMember(MemberDto dto, String oldPassword) {
 		// db에서 member 읽어서
 		MemberDto oldMember = mapper.selectMemberById(dto.getId());
-		
+
 		String encodedPW = oldMember.getPassword();
-		
+
 		// 기존password가 일치할 때만 계속 진행
 		if (passwordEncoder.matches(oldPassword, encodedPW)) {
-			
+
 			// 암호 인코딩
 			dto.setPassword(passwordEncoder.encode(dto.getPassword()));
-			
+
 			return mapper.updateMember(dto) == 1;
 		}
-		
+
 		return false;
 	}
 
 	public boolean hasMemberId(String id) {
 		return mapper.countMemberId(id) > 0;
 	}
-	
+
 	public boolean hasMemberEmail(String email) {
 		return mapper.countMemberEmail(email) > 0;
 	}
@@ -102,11 +102,11 @@ public class MemberService {
 	public boolean hasMemberNickName(String nickName) {
 		return mapper.countMemberNickName(nickName) > 0;
 	}
-	
+
+	public boolean setAddress(MemberDto dto) {
+		
+		return mapper.updateAddress(dto.getId(), dto) == 1;
+
+	}
+
 }
-
-
-
-
-
-
